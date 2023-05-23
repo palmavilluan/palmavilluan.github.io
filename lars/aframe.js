5fd701de66b77e2289db724f3a0eadf928396683
Slender4fun <74436458+Slender4fun@users.noreply.github.com>
2023-05-23 14:39:24 +0200

    finally working gallery

--- lars/aframe.js
+++ lars/aframe.js
@@ -1,53 +1,27 @@
-AFRAME.registerShader('gradient', {
-    schema: {
-      topColor: {type: 'vec3', default: '1 0 0', is: 'uniform'},
-      bottomColor: {type: 'vec3', default: '0 0 1', is: 'uniform'},
-      offset: {type: 'float', default: '400', is: 'uniform'},
-      exponent: {type: 'float', default: '0.6', is: 'uniform'}
-    },
-    vertexShader: [
-      'varying vec3 vWorldPosition;',
+path = "img/";
+i = 1;
+max = 6;
+imageWidth = 5;
+gapWidth = 0.5;
+
+galleryCenter = (max+1)/2;
+
+
+while (i <= max) {
+
+  console.log(path + i);
+  newImg = document.createElement("a-box");
   
-      'void main() {',
-  
-      'vec4 worldPosition = modelMatrix * vec4( position, 1.0 );',
-      'vWorldPosition = worldPosition.xyz;',
-  
-       'gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0 );',
-  
-      '}'
-    ].join('\n'),
-    fragmentShader: [
-      'uniform vec3 bottomColor;',
-      'uniform vec3 topColor;',
-      'uniform float offset;',
-      'uniform float exponent;',
-      'varying vec3 vWorldPosition;',
-      'void main() {',
-      ' float h = normalize( vWorldPosition + offset ).y;',
-      ' gl_FragColor = vec4( mix( bottomColor, topColor, max( pow( max(h, 0.0 ), exponent ), 0.0 ) ), 1.0 );',
-      '}'
-    ].join('\n')
-  });
-  
-  AFRAME.registerPrimitive('a-gradient-sky', {
-    defaultComponents: {
-      geometry: {
-        primitive: 'sphere',
-        radius: 5000,
-        segmentsWidth: 64,
-        segmentsHeight: 20
-      },
-      material: {
-        shader: 'gradient'
-      },
-      scale: '-1 1 1'
-    },
-  
-    mappings: {
-      topColor: 'material.topColor',
-      bottomColor: 'material.bottomColor',
-      offset: 'material.offset',
-      exponent: 'material.exponent'
-    }
-  });
+  newImg.setAttribute("src", path + "img" + i + ".jpg");
+  newImg.setAttribute("id", "image " + i);
+  newImg.setAttribute("position", (i-(max+1)/2)*(imageWidth+gapWidth) + " 2 -5");
+  newImg.setAttribute("color", "white");
+  newImg.setAttribute("class", "image");
+  newImg.setAttribute("width", imageWidth);
+  newImg.setAttribute("height", 5);
+  newImg.setAttribute("depth", "0.01%");
+
+  console.log(newImg);
+  document.getElementById("aScene").appendChild(newImg);
+  i++;
+}
