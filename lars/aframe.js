2d11d907c5ddd09be5a510966f5b80e436b07327
Slender4fun <74436458+Slender4fun@users.noreply.github.com>
2023-05-25 12:14:08 +0200

    room and lights

--- lars/aframe.js
+++ lars/aframe.js
@@ -4,6 +4,9 @@ max = 6;
 imageWidth = 5;
 gapWidth = 0.5;
 
+areaLightWidth = 5;
+areaLightHeight = 5;
+
 galleryCenter = (max+1)/2;
 
 
@@ -20,16 +23,44 @@ while (i <= max) {
   newImg.setAttribute("height", 5);
   newImg.setAttribute("depth", "0.01%");
   newImg.setAttribute("shadow", "cast: true; receive: true");
+  
 
   document.getElementById("aScene").appendChild(newImg);
+
+
+// individual light for each image
+newLight = document.createElement("a-entity");
+
+newLight.setAttribute("area-light", "color: white; intensity: 0.5; castShadow: true; target: #image"+i+"; angle: 20; width: " + areaLightWidth + "; height:" + areaLightHeight + ";");
+newLight.setAttribute("position", (i-(max+1)/2)*(imageWidth+gapWidth)-(areaLightWidth/2) + " 3 -9.79");
+newLight.setAttribute("id", "light"+i);
+
+document.getElementById("aScene").appendChild(newLight);
+
+  // <a-entity
+  // id="deckenlicht" 
+  // area-light="
+  // intensity: 1;
+  // width: 40; 
+  // height: 1; 
+  // color: white; 
+  // penumbra: 1;" 
+  // position="-20 5.99 -9.9" 
+  // rotation="-90 0 0"
+  // ></a-entity>
+
+
+
+
+
 
 // individual light for each image
   newLight = document.createElement("a-entity");
 
-  newLight.setAttribute("light", "type: spot; penumbra: 0.1; color: white; intensity: 1; castShadow: true; target: #image"+i+"; angle: 20;");
-  newLight.setAttribute("position", (i-(max+1)/2)*(imageWidth+gapWidth) + " 6 -3");
+  // newLight.setAttribute("light", "type: spot; penumbra: 0.1; color: white; intensity: 1; castShadow: true; target: #image"+i+"; angle: 20;");
+  // newLight.setAttribute("position", (i-(max+1)/2)*(imageWidth+gapWidth) + " 6 -3");
 
-  document.getElementById("aScene").appendChild(newLight);
+  // document.getElementById("aScene").appendChild(newLight);
 
   i++;
 
